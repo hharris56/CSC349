2c1c4d22abe88c49b4eb46eec201204f3ccba011
hharris56 <hvharris@calpoly.edu>
2019-05-22 14:29:30 -0400

    updated, Hunter Harris

--- proj4/ChangeMaker.java
+++ proj4/ChangeMaker.java
@@ -12,14 +12,16 @@ public class ChangeMaker{
 
     public static void main(String[] args){
         Scanner cmdline = new Scanner(System.in);
-        System.out.print("Enter # of coins and their value in DECREASING order: ");
+        System.out.print("Enter # of coins then their value in DECREASING order: ");
         int k = cmdline.nextInt();
         int[] d = new int[k];
         for (int i=0;i<k;i++){
             d[i] = cmdline.nextInt();
         }
-        System.out.print("Enter starting n: ");
+        System.out.print("Enter change amount: ");
         int n = cmdline.nextInt();
+        int[] change = change_DP(n, d);
+        printArray(change);
     }
 
     public static int[] change_DP(int n, int[] d){
@@ -29,7 +31,7 @@ public class ChangeMaker{
         C[0] = 0;                       // initial value, change for 0 is zero coins
 
         for (int j=0;j<n;j++){                                  // iterate through all values until given amount
-            int min = 0;                                            // minimum value reference
+            int min = 0;                                        // minimum value reference
             for (int i=0;i<d.length;i++){                       // iterate through coin values from d
                 if (j > d[i] && (C[j - d[i]] + 1 < min)) {      // check if valid && check if less than min
                     min = C[j - d[i]] + 1;                      // update min reference
@@ -39,5 +41,13 @@ public class ChangeMaker{
             B[A[j]]++;                                          // increment chosen coin
         }
     return B;
+    }
+
+    private static void printArray(int[] A){
+        System.out.print("[");
+        for (int i=0;i<A.length-1;i++){
+            System.out.print(A[i] + ", ");
+        }
+        System.out.println(A[A.length-1] + "]");
     }
 }
