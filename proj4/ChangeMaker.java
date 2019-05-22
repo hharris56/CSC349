7a239943f6b504638419ddf5434956f7e75b2520
hharris56 <hvharris@calpoly.edu>
2019-05-22 13:49:48 -0400

    updated, Hunter Harris

--- proj4/ChangeMaker.java
+++ proj4/ChangeMaker.java
@@ -23,6 +23,21 @@ public class ChangeMaker{
     }
 
     public static int[] change_DP(int n, int[] d){
-        return d;
+        int[] C = new int[n];           // holds # of coins needed to change for each value
+        int[] A = new int[n];           // holds index of selected coin from d array
+        int[] B = new int[d.length];    // holds count for each coins value in d array
+        C[0] = 0;                       // initial value, change for 0 is zero coins
+
+        for (int j=0;j<n;j++){                                  // iterate through all values until given amount
+            int min = 0;                                            // minimum value reference
+            for (int i=0;i<d.length;i++){                       // iterate through coin values from d
+                if (j > d[i] && (C[j - d[i]] + 1 < min)) {      // check if valid && check if less than min
+                    min = C[j - d[i]] + 1;                      // update min reference
+                    A[j] = i;                                   // hold index of coin used to acheive min
+                }
+            }
+            B[A[j]]++;                                          // increment chosen coin
+        }
+    return B;
     }
 }
