929ab2b698e9dd9eff1dbdfb6fb166758b3cb8d2
hharris56 <hvharris@calpoly.edu>
2019-05-24 02:15:30 -0400

    Revert "updated, Hunter Harris"
    This reverts commit 42fa447c4c9924028d701d3625057a333017a888.

--- proj4/ChangeMaker.java
+++ proj4/ChangeMaker.java
@@ -20,7 +20,7 @@ public class ChangeMaker{
         }
         System.out.print("Enter change amount: ");
         int n = cmdline.nextInt();
-        int[] change = change_DP(n+1, d);
+        int[] change = change_DP(n, d);
         printArray(change);
     }
 
@@ -30,32 +30,17 @@ public class ChangeMaker{
         int[] B = new int[d.length];    // holds count for each coins value in d array
         C[0] = 0;                       // initial value, change for 0 is zero coins
 
-        for (int j=1;j<n;j++){                                  // iterate through all values until given amount
-            int min = Integer.MAX_VALUE;                        // minimum value reference
-            int index = 0;                                      // index of minimum value
+        for (int j=0;j<n;j++){                                  // iterate through all values until given amount
+            int min = 0;                                        // minimum value reference
             for (int i=0;i<d.length;i++){                       // iterate through coin values from d
-                if (j >= d[i]) {                                // check if valid && check if less than min
-                    if (C[j - d[i]] + 1 < min){
-                        min = C[j - d[i]] + 1;                  // update min reference
-                        index = i;                              // hold index of coin used to acheive min
-                    }
+                if (j > d[i] && (C[j - d[i]] + 1 < min)) {      // check if valid && check if less than min
+                    min = C[j - d[i]] + 1;                      // update min reference
+                    A[j] = i;                                   // hold index of coin used to acheive min
                 }
             }
-            C[j] = min;
-            A[j] = index;
+            B[A[j]]++;                                          // increment chosen coin
         }
-        buildResult(A, B, d);
     return B;
-    }
-
-    // method to build result list (B) from list of coin indexes (A)
-    private static void buildResult(int[] A, int[] B, int[] d){
-        int n = A.length-1;             // get last index
-        while (n>0){
-            int coinIndex = A[n];       // get index from index array
-            B[coinIndex]++;             // increment related index
-            n -= d[coinIndex];          // decrement n by coin value
-        }
     }
 
     private static void printArray(int[] A){
