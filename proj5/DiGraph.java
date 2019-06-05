92befe9afbd0f5caebfcf1b47ebc2cdfdae49b1a
hharris56 <hvharris@calpoly.edu>
2019-06-05 18:44:25 -0400

    final upload for proj5

--- proj5/DiGraph.java
+++ proj5/DiGraph.java
@@ -1,6 +1,6 @@
 // Garrett DeAngelis, Hunter Harris
 // CSC 349, Spring 2019
-// Project 5 (graphs)
+// Project 5 (Graphs)
 
 import java.util.LinkedList;
 import java.util.Iterator;
@@ -9,179 +9,264 @@ import java.lang.Error;
 public class DiGraph {
    private static LinkedList<Integer>[] graph;
 
+   // constructor
    public DiGraph(int N) {
       graph = new LinkedList[N];
-      for (int i = 0; i < N; i++) {
+      for(int i = 0; i<N; i++) {                                        // initialize vertices
          LinkedList<Integer> vertices = new LinkedList<Integer>();
          graph[i] = vertices;
       }
    }
 
-   // HH - made changed indexOf(to) and addLast(to) to indexOf(to-1) and
-   // addLast(to-1) for
-   // consistancy with numbering (always add 1 to get natural index)
-   public void addEdge(int from, int to) {
-      if (graph[from - 1].indexOf(to - 1) < 0) { // then edge is missing from that graph, add it to the beginning
-         graph[from - 1].addLast(to - 1);
-      }
-      // System.out.println("edge from " + from + " to " + to);
-   }
-
-   public void deleteEdge(int from, int to) {
-      int index = graph[from - 1].indexOf(to - 1);
-      if (index > -1) { // slight change for 1 less comparison (> as opposed to >=)
-         graph[from - 1].remove(index);
+   // adds edge to DiGraph
+   public static void addEdge(int from, int to) {
+      if(graph[from-1].indexOf(to) < 0) {                               // check if edge exists, add if it doesnt
+         graph[from-1].addLast(to);
+         System.out.println("edge added from " + from + " to " + to);
       }
    }
 
-   public int edgeCount() {
+   // removes edge from DiGraph
+   public static void deleteEdge(int from, int to) {
+      int index = graph[from-1].indexOf(to);
+      if(index > -1) {                                                  // check if edge exists, delete if it does
+         graph[from-1].remove(index);
+         System.out.println("edge removed from " + from + " to " + to);
+      }
+   }
+
+   // returns total number of edges in DiGraph
+   public static int edgeCount() {
       int edgeCount = 0;
-      for (int i = 0; i < graph.length; i++) {
+      for(int i = 0; i<graph.length; i++) {
          edgeCount = edgeCount + graph[i].size();
       }
       return edgeCount;
    }
 
-   public int vertexCount() {
+   // returns total number of vertices in DiGraph
+   public static int vertexCount()
+   {
       return graph.length;
    }
 
-   public static void oldPrint() {
-      // prints each vertex and its connected nodes
-      System.out.println();
-      System.out.println("The graph is the following: ");
-      for (int i = 0; i < graph.length; i++) {
-         System.out.print(i + 1 + " is connected to: ");
-         // System.out.print((i+1) + " is connected to: ");
-         for (int j = 0; j < graph[i].size() - 1; j++) {
-            System.out.print((graph[i].get(j)) + ", ");
-         }
-         if (graph[i].size() > 0)
-            System.out.print(graph[i].getLast());
-         System.out.println();
-      }
-   }
-
+   // prints out DiGraph
    public void print() {
       System.out.println();
       for (int i = 0; i < graph.length; i++) {
          System.out.print((i + 1) + " is connected to: ");
-         boolean first = true;
-         for (int j = 0; j < graph[i].size(); j++) {
+         boolean first = true;                                 // used for string formatting
+         for (int j = 0; j < graph[i].size(); j++) {           // only prints comma if not first value
             if (first != true) {
                System.out.print(", ");
             } else {
                first = false;
             }
-            System.out.print((graph[i].get(j) + 1));
+            System.out.print(graph[i].get(j));
          }
          System.out.println();
       }
    }
 
+   // counts the number of connections a vertex RECEIVES
    // used for topological sorting
-   private int[] indegrees() {
+   private static int[] indegrees() {
       int n = graph.length;
       int[] in = new int[n];
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < graph[i].size();j++) {
-            in[graph[i].get(j)]++;
+      for(int i = 0; i<n; i++)
+         in[i] = 0;
+
+      for(int i = 0; i<n; i++) {
+         for(int j = 0; j<graph[i].size(); j++) {
+            in[graph[i].get(j)-1]++;
          }
       }
       return in;
    }
-/*
-   // returns topologically sorted vertices
-   public int[] topSort() {
-      LinkedList<Integer> queue = new LinkedList<Integer>();
-      int n = graph.length;
-      int[] in = indegrees();
-      int[] A = new int[n];
-      for (int i = 0; i < n; i++) {
-         if (in[i] == 0)
-            queue.add(i);
-      }
-      int i = 0;
-      while (queue.size() != 0) {
-         int temp = queue.remove();
-         A[i] = temp;
-         i++;
-         for (int j = 0; j < graph[temp].size(); j++) {
-            in[j] = in[j] - 1;
-            if (in[j] == 0)
-               queue.add(j);
-         }
-      }
-      if (i == n + 1)
-         throw new ArithmeticException("Graph is cyclical!");
-      return A;
-   }
 
-   public int[] topSort2() {
+   // topological sort of the graph
+   public static int[] topSort() {
       int N = graph.length;
       int[] in = indegrees();
       int[] A = new int[N];
       LinkedList<Integer> queue = new LinkedList<Integer>();
 
-      System.out.println("N:" + N);
-      for (int u = 0; u < N; u++) {
-         if (in[u] == 0)
-            queue.offer(u);
+      for(int u = 0; u<N; u++) {       // start by adding all vertices of 0 degrees to queue
+         if(in[u] == 0)
+            queue.offer(u+1);
       }
 
       int i = 0;
-      System.out.println("queue size: " + queue.size());
-      while (!queue.isEmpty()) {
+      while(!queue.isEmpty()) {
          int u = queue.remove();
-         A[i] = u;
-         i++;
-         System.out.print(u + ": ");
-         for (int v = 0; v < graph[u].size(); v++) {
-            System.out.print(+graph[u].get(v) - 1 + " ");
-            in[graph[u].get(v) - 1]--;
-            // in[v] = in[v]-1;
-            if (in[graph[u].get(v) - 1] == 0)
-               queue.offer(v);
+         A[i] = u;                                          // place vertex in array
+         i++;                                               // increment index for next vertex
+         for(int v = 0; v<graph[u-1].size(); v++) {
+            in[graph[u-1].get(v)-1]--;                      // decrement indegree of connected vertices
+            if(in[graph[u-1].get(v)-1] == 0)                // if vertex now has indegree of 0, add to queue
+               queue.offer(graph[u-1].get(v));
          }
-         System.out.println();
       }
 
-      System.out.println("N length: " + N);
-      System.out.println("i: " + i);
-      // if(i != N+1)
-      // throw new ArithmeticException("Graph is cyclical!");
-
+      if(i != N)                                            // check for cyclic graph (topological sort impossible)
+         throw new ArithmeticException("Graph is cyclic!");
       return A;
    }
-*/
-   public int[] topSort(){
-      int n = graph.length;
-      int[] in = indegrees();
-      int[] result = new int[n];
-      LinkedList<Integer> Q = new LinkedList();
 
-      for (int i=0;i<n;i++){
-         if (in[i] == 0){
-            Q.addLast(i);
-         }
+   private class VertexInfo {
+      int distance, parent;   // parent in NATURAL INDEXING
+      public VertexInfo(int distance1, int predeccesor) {
+         distance = distance1;
+         parent = predeccesor;
       }
+      public String toString(int num)
+      {
+         return "node " + num + ": " + "distance: " + distance + " parent: " + parent;
+      }
+   }
 
-      int i = 0;
-      while (!Q.isEmpty()){
-         Integer curr = Q.removeFirst();
-         result[i] = curr + 1;
-         for (int j=0;j<graph[curr].size();j++){
-            int node = graph[curr].get(j);
-            in[node]--;
-            if (in[node] == 0){
-               Q.addLast(node);
+   // Breadth-first search of graph
+   // calculates BFS of the graph starting from s vertex
+   // returns array of VertexInfo
+   private VertexInfo[] BFS(int s) {
+      int n = graph.length;
+      VertexInfo[] vertices = new VertexInfo[n];
+      // use addLast to add, removeFirst to remove
+      LinkedList<Integer> queue = new LinkedList<Integer>();
+
+      for(int i = 0; i<n; i++) {
+         VertexInfo vertex = new VertexInfo(-1, -1);
+         vertices[i] = vertex;
+      }
+      vertices[s-1].distance = 0;                                          // convert s from NATURAL to WHOLE for indexing
+      queue.addLast(s);
+      while(!queue.isEmpty()) {
+         int u = queue.removeFirst();                                      // dequeue
+         for(int v = 0; v<graph[u-1].size(); v++) {
+            int adjIndex = graph[u-1].get(v)-1;
+            if(vertices[adjIndex].distance == -1) {                        // if distance is not yet set
+               vertices[adjIndex].distance = vertices[u-1].distance+1;     // set distance to current vertex + 1
+               vertices[adjIndex].parent = u;                              // set parent to current vertex
+               queue.addLast(adjIndex+1);                                  // increment by one because it represents the node, not needed for index
             }
          }
-         i++;
       }
-      if (i != n){
-         throw new IllegalArgumentException();
-      }
-      return result;
+      return vertices;
    }
+
+   // checks if path exists between two vertices
+   public boolean isTherePath(int from, int to) {
+      VertexInfo[] vertices = BFS(from);
+      for(int i = 0; i<vertices.length; i++) {
+         System.out.println(vertices[i].toString(i+1));
+      }
+      if(vertices[to-1].distance == -1)
+         return false;
+      return true;
+   }
+
+   public int lengthOfPath(int from, int to) {
+      //test to see if there is a validPath
+      if(from == to)
+         return 0;
+
+      if(isTherePath(from, to) == false)
+         return -1;
+
+      int minLength = 1;                        //init minLength to something really large by default
+      VertexInfo[] vertices = BFS(from);
+
+      VertexInfo current = vertices[to-1];     //ending node --> retrace our steps to target
+      int currentIndex = to;
+
+      while(current.parent != from) {
+         System.out.println("current: " + currentIndex + " parent: " + current.parent);
+         minLength++;
+         current = vertices[current.parent-1];
+         currentIndex = current.parent;
+      }
+      return minLength;
+   }
+
+   public void printPath(int from, int to) {
+      if(isTherePath(from, to) == false || from == to)   //handles the edge cases
+         System.out.println("There is no path");
+      else {
+         int pathLength = lengthOfPath(from, to);
+         int[] backwardList = new int[pathLength+1];     //create a backwards list that is one greater than the paths
+         //System.out.println("backwardList length: " + backwardList.length);
+         VertexInfo[] vertices = BFS(from);
+
+         VertexInfo current = vertices[to-1];
+         int currentIndex = to;
+         int index = pathLength;
+
+         while(current.parent != -1) {
+            backwardList[index] = currentIndex;
+            System.out.println("inserting " + currentIndex + " to backwardList at index: " + index);
+            index--;
+            currentIndex = current.parent;
+            current = vertices[current.parent-1];
+         }
+         backwardList[index] = currentIndex;
+
+         for(int i = 0; i<pathLength; i++) {
+            System.out.print(backwardList[i] + " --> ");
+         }
+         System.out.println(backwardList[pathLength]);
+      }
+   }
+
+   // Class TreeNode -----------------------------------------
+   private class TreeNode{
+      int vert;
+      LinkedList<TreeNode> children;
+
+      public TreeNode(int n){
+         this.vert = n;
+         this.children = new LinkedList<TreeNode>();
+      }
+   }
+
+   private TreeNode buildTree(int s){
+      VertexInfo[] vertInfo = BFS(s);           // obtain vertex data using BFS
+      int n = vertInfo.length;
+      TreeNode[] nodes = new TreeNode[n];
+
+      for (int i=0;i<n;i++){
+         nodes[i] = new TreeNode(i+1); // <--- set vertices using NATURAL numbers
+      }
+
+      for (int i=0;i<n;i++){
+         int parentIndex = vertInfo[i].parent - 1; // <--- subtract to convert NATURAL to WHOLE
+         if (parentIndex > -1) {
+            TreeNode parent = nodes[parentIndex];
+            TreeNode child = nodes[i];
+            parent.children.add(child);
+         }
+      }
+
+      return nodes[s-1];
+   }
+
+   // printTree wrapper
+   // calls buildTree and passes root to printTreeRecur
+   public void printTree(int s){
+      System.out.println();
+      TreeNode root = buildTree(s);
+      printTreeRecur(root, 0);
+   }
+
+   // recursively prints tree using TreeNode type objects
+   // level determines how far to indent
+   private void printTreeRecur(TreeNode node, int level){
+      for (int i=0;i<level;i++){
+         System.out.print("    ");                             // indent to match level
+      }
+      System.out.println(node.vert);                           // print node value (vertex number)
+      for (int i=0;i<node.children.size();i++){
+         printTreeRecur(node.children.get(i), level+1);  // recursive call on children
+      }
+   }
+
 }
--- proj5/DiGraphTest.java
+++ proj5/DiGraphTest.java
@@ -1,10 +1,9 @@
 // Garrett DeAngelis, Hunter Harris
 // CSC 349, Spring 2019
-// Project 5 (graphs)
+// Project 5 (Graphs)
 
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.lang.IllegalArgumentException;
 
 public class DiGraphTest
 {
@@ -13,8 +12,9 @@ public class DiGraphTest
       Scanner scan = new Scanner(System.in);
       System.out.println("Enter the number of vertices:");
       int numVertices = scan.nextInt();
+      scan.nextLine();
       DiGraph graph = new DiGraph(numVertices);
-      
+
       //print the menu
       System.out.println("Choose one of the following operations:");
       System.out.println("- add edge (enter a)");
@@ -22,22 +22,26 @@ public class DiGraphTest
       System.out.println("- edge count (enter e)");
       System.out.println("- vertex count (enter v)");
       System.out.println("- print graph (enter p)");
-      System.out.println("- topological sort (enter t)");
+      System.out.println("- topological sort");
       System.out.println("- Quit (enter q)");
 
       boolean run = true;
       while(run)
       {
-         String response = scan.next();
-         if(response.length() != 1)
-            //System.out.println("too long!!!");
+         //still issue with a 4 5 input
+         String response = scan.nextLine();
+         if(response.length() != 1) {
+            System.out.println("answer too long");
             response = "x";
+         }
+
          switch(response)
          {
             case "a":
                System.out.println("Enter the vertices you would like to add an edge between (to, from):");
                int to = scan.nextInt();
                int from = scan.nextInt();
+               scan.nextLine();
                graph.addEdge(to, from);
                break;
 
@@ -45,6 +49,7 @@ public class DiGraphTest
                System.out.println("Enter the vertices you would like to delete an edge between (from, to):");
                from = scan.nextInt();
                to = scan.nextInt();
+               scan.nextLine();
                graph.deleteEdge(from, to);
                break;
 
@@ -57,13 +62,44 @@ public class DiGraphTest
                break;
 
             case "p":
-               System.out.println("The graph is the following: ");
                graph.print();
                break;
 
             case "t":
-               System.out.println("Topological sort results: ");
                printTopo(graph);
+               break;
+
+            case "i":
+               System.out.println("Enter the vertices you would like to check the path (from, to):");
+               from = scan.nextInt();
+               to = scan.nextInt();
+               scan.nextLine();
+               boolean result = graph.isTherePath(from, to);
+               System.out.println("is there a path: " + result);
+               break;
+
+            case "l":
+               System.out.println("Enter the vertices you would like to check the path (from, to):");
+               from = scan.nextInt();
+               to = scan.nextInt();
+               scan.nextLine();
+               int length = graph.lengthOfPath(from, to);
+               System.out.println("The length of the path is: " + length);
+               break;
+
+            case "s":
+               System.out.println("Enter the vertices you would like to check the path (from, to):");
+               from = scan.nextInt();
+               to = scan.nextInt();
+               scan.nextLine();
+               graph.printPath(from, to);
+               break;
+
+            case "b":
+               System.out.print("Enter source vertex: ");
+               int source = scan.nextInt();
+               scan.nextLine();
+               graph.printTree(source);
                break;
 
             case "q":
@@ -79,23 +115,19 @@ public class DiGraphTest
          System.out.println("Enter a menu choice: ");
       }
    }
+
    //takes in a list of vertices and prints them in the right format
-   private static void printTopo(DiGraph graph){
+   private static void printTopo(DiGraph graph) {
+      System.out.print("topo sorting: ");
       try {
-         int[] sorted = graph.topSort();
-         printArray(sorted);
+         int[] result = graph.topSort();
+         for(int i = 0; i<result.length-1; i++) {
+            System.out.print((result[i]) + ", ");
+         }
+         System.out.println(result[result.length-1]);
       }
-      catch (IllegalArgumentException e){
-         System.out.println("The graph is cyclic");
+      catch(ArithmeticException e) {
+         System.out.println("Graph is cyclical!");
       }
-
-   }
-
-   public static void printArray(int[] A){
-      System.out.print("[");
-      for (int i=0;i<A.length-1;i++){
-         System.out.print(A[i] + ", ");
-      }
-      System.out.println(A[A.length-1] + "]");
    }
 }
